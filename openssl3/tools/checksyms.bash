efc1ac043ea07641f0aad807dabefa3f0c9880a9
Dan McDonald <danmcd@mnx.io>
2023-07-21 11:15:42 -0400

    OS-8475 OpenSSL to 3.0.9, and CVE-2023-2650 to 1.0.2
    Reviewed by: Brian Bennett <brian.bennett@mnx.io>

--- openssl3/tools/checksyms.bash
+++ openssl3/tools/checksyms.bash
@@ -29,7 +29,7 @@ ALLOWED_SYMS="$ALLOWED_SYMS _init _lib_version _PROCEDURE_LINKAGE_TABLE_"
 # Because we let OpenSSL 3 do the library renaming, it includes this in the
 # symbol list. OpenSSL 3 will be adding to this as it adds new symbols.
 ALLOWED_SYMS="$ALLOWED_SYMS OPENSSL_SMARTOS_3.0.0 OPENSSL_SMARTOS_3.0.3"
-ALLOWED_SYMS="$ALLOWED_SYMS OPENSSL_SMARTOS_3.0.8"
+ALLOWED_SYMS="$ALLOWED_SYMS OPENSSL_SMARTOS_3.0.8 OPENSSL_SMARTOS_3.0.9"
 
 NM=/usr/bin/nm
 PREFIX="^sunw_"
